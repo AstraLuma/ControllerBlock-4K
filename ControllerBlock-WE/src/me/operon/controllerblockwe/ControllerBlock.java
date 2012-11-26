e9c27dda4cbb7f57ece126d43275080a50e6f692
James Bliss <james.bliss@astro73.com>
2012-11-26 18:06:54 -0500

    Some minor formatting.

--- ControllerBlock-WE/src/me/operon/controllerblockwe/ControllerBlock.java
+++ ControllerBlock-WE/src/me/operon/controllerblockwe/ControllerBlock.java
@@ -133,9 +133,9 @@ public class ControllerBlock extends JavaPlugin implements Runnable {
 								}
 							}
 						} catch (IncompleteRegionException e) {
-							player.sendMessage("Incomplete region. Finish your selection first.");
+							player.sendMessage("§cIncomplete region. Finish your selection first.");
 						} catch (NonBukkitWorld e) {
-							player.sendMessage("WorldEdit gave me a non-Bukkit world. Contact your admin.");
+							player.sendMessage("§cWorldEdit gave me a non-Bukkit world. Contact your admin.");
 						}
 						sender.sendMessage(affected + " blocks added to ControllerBlock");
 					} else if (args[0].equalsIgnoreCase("remove")
@@ -155,9 +155,9 @@ public class ControllerBlock extends JavaPlugin implements Runnable {
 								}
 							}
 						} catch (IncompleteRegionException e) {
-							player.sendMessage("Incomplete region. Finish your selection first.");
+							player.sendMessage("§cIncomplete region. Finish your selection first.");
 						} catch (NonBukkitWorld e) {
-							player.sendMessage("WorldEdit gave me a non-Bukkit world. Contact your admin.");
+							player.sendMessage("§cWorldEdit gave me a non-Bukkit world. Contact your admin.");
 						}
 						sender.sendMessage(affected + " blocks removed from ControllerBlock");
 					}
